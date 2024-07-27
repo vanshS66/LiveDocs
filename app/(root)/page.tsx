e6ea0e18447c772499d4678ddf2f75df69cc5801
LegendLeaks <v.s.vansh.sehrawat@gmail.com>
2024-07-27 02:15:48 -0400

    added local files

---
+++ app/(root)/page.tsx
@@ -0,0 +1,41 @@
+import Header from '@/components/Header'
+import { Button } from '@/components/ui/button'
+import { SignedIn, UserButton } from '@clerk/nextjs'
+import Image from 'next/image'
+import React from 'react'
+
+const Home = () => {
+  const documents = [];
+
+  return (
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          Notification
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+        </div>
+      </Header>
+
+      {documents.length > 0 ? (
+        <div>
+
+        </div>
+      ): (
+        <div className="document-list-empty">
+          <Image 
+            src="/assets/icons/doc.svg"
+            alt="Document"
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
+        </div>
+      )}
+
+    </main>
+  )
+}
+
+export default Home
---
+++ lib/actions/room.actions.ts
@@ -0,0 +1,35 @@
+'user server';
+
+import { nanoid } from 'nanoid'
+import { liveblocks } from '../liveblocks';
+import { parseStringify } from '../utils';
+import { revalidatePath } from 'next/cache';
+
+export const createDocument = async ({ userId, email } :
+    CreateDocumentParams) => {
+        const roomId = nanoid();
+
+        try {
+            const metadata = {
+                creatorId: userId,
+                email,
+                title: 'Untitled'
+            }
+
+            const usersAccesses: RoomAccesses = {
+                [email]: ['room:write']
+            }
+
+            const room = await liveblocks.createRoom(roomId, {
+                metadata,
+                usersAccesses,
+                defaultAccesses: []
+              });
+              
+              revalidatePath('/');
+
+              return parseStringify(room);
+        } catch(error) {
+            console.log("error happened while creating a room: ${error})");
+        }
+}
